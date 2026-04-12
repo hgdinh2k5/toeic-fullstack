b187d0c31416d621ca2e51e2a157a0188454c0fe
hgdinh2k5 <Hhgadh123@gmail.com>
2026-04-12 21:48:12 +0700

    Initial commit: Start TOEIC project with Spring Boot and Thymeleaf

---
+++ src/main/java/org/example/toeicfullstack/ToeicFullstackApplication.java
@@ -0,0 +1,13 @@
+package org.example.toeicfullstack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToeicFullstackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ToeicFullstackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/toeicfullstack/ToeicFullstackApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.toeicfullstack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToeicFullstackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
