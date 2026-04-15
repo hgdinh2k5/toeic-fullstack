3baf2f6b843cfc1fcb79a8fd642f46112d198df0
hgdinh2k5 <Hhgadh123@gmail.com>
2026-04-15 15:01:43 +0700

    entity

---
+++ src/main/java/org/example/toeicfullstack/config/ConfigCloundinary.java
@@ -0,0 +1,22 @@
+package org.example.toeicfullstack.config;
+
+import com.cloudinary.Cloudinary;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Configuration
+public class ConfigCloundinary {
+
+    @Bean
+    public Cloudinary configKey() {
+        Map<String, String> config = new HashMap<String, String>();
+        config.put("cloud_name", "dybz8iuyt");
+        config.put("api_key", "934197112982238");
+        config.put("api_secret", "qu3OvcrmNO-hd1-7b_0r6bP1Gx8");
+        return new Cloudinary(config);
+    }
+
+}
---
+++ src/main/java/org/example/toeicfullstack/entity/enums/QuestionType.java
@@ -0,0 +1,5 @@
+package org.example.toeicfullstack.entity.enums;
+
+public enum QuestionType {
+    MULTIPLE_CHOICE, FILL_IN_BLANK, ORDERING
+}
