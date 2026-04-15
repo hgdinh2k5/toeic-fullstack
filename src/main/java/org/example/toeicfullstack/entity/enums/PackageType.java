dfc03d62bc60f574f61a3227ab1204e323f0b9bc
hgdinh2k5 <Hhgadh123@gmail.com>
2026-04-15 22:37:18 +0700

    entity

--- src/main/java/org/example/toeicfullstack/entity/enums/PackageType.java
+++
@@ -1,5 +0,0 @@
-package org.example.toeicfullstack.entity.enums;
-
-public enum PackageType {
-    BASIC, PREMIUM, ENTERPRISE
-}
--- src/main/java/org/example/toeicfullstack/entity/enums/VocabType.java
+++
@@ -1,5 +0,0 @@
-package org.example.toeicfullstack.entity.enums;
-
-public enum VocabType {
-    NOUN, VERB, ADJECTIVE, ADVERB, PHRASE
-}
