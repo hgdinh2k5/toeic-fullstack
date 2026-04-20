c91634bbb67be7fd9b99dca99d207de7a1d9eaa8
maiquynhvnlhb <maiquynhvnlhb@gmail.com>
2026-04-20 13:21:27 +0700

    login

--- src/main/java/org/example/toeicfullstack/entity/FillBlankQuestion.java
+++ src/main/java/org/example/toeicfullstack/entity/FillBlankQuestion.java
@@ -14,9 +14,9 @@ public class FillBlankQuestion extends Question {
     @Column(columnDefinition = "TEXT")
     private String correctText;
 
+    @Column(columnDefinition = "TEXT")
     private String audioUrl;
 
     @Column(columnDefinition = "TEXT")
     private String hint;
 }
-
